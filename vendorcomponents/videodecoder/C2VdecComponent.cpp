3c09e5cdb1e38610fcb56f12f9db09de04f6eb0e
Jian Wang <jian.wang@amlogic.com>
2024-06-24 13:55:27 +0800

    codec2: CF1 Optimize low latency delay [1/1]
    PD#SWPL-174824
    PD#TV-123417
    
    Problem:
    Low latency have delay about 300+ms, it is
    too long
    
    Solution:
    Use game mode for low latency
    
    Verify:
    ohm
    
    Change-Id: Id6f58e5835a3a3b93e2c9a8adaf667062d453559
    Signed-off-by: Jian Wang <jian.wang@amlogic.com>

--- vendorcomponents/videodecoder/C2VdecComponent.cpp
+++ vendorcomponents/videodecoder/C2VdecComponent.cpp
@@ -3126,7 +3126,7 @@ c2_status_t C2VdecComponent::start() {
     mTaskRunner->PostDelayedTask(FROM_HERE,
         ::base::Bind(&C2VdecComponent::checkPreempting, ::base::Unretained(this)),
         ::base::TimeDelta::FromMilliseconds(100));
-    if (mIntfImpl->mVendorGameModeLatency->enable && mDeviceUtil) {
+    if ((mIntfImpl->mVendorGameModeLatency->enable || mIntfImpl->mLowLatencyMode->value) && mDeviceUtil) {
         mDeviceUtil->setGameMode(true);
     }
     mVdecComponentStopDone = false;
@@ -3148,7 +3148,7 @@ c2_status_t C2VdecComponent::stop() {
     if (!(state == State::RUNNING || state == State::ERROR)) {
         return C2_OK;  // Component is already in stopped state.
     }
-    if (mIntfImpl->mVendorGameModeLatency->enable && mDeviceUtil) {
+    if ((mIntfImpl->mVendorGameModeLatency->enable || mIntfImpl->mLowLatencyMode->value) && mDeviceUtil) {
         mDeviceUtil->setGameMode(false);
     }
 
