16eab93c1c05fce606d0cd7e7c48ea4633409575
jianchuan.ping <jianchuan.ping@amlogic.com>
2025-07-18 15:56:47 +0800

    Sync code gape in ref server
    Change-Id: I77c72ef1094bfc0d45c020c9544f44b2ef0249c4
    Signed-off-by: jianchuan.ping <jianchuan.ping@amlogic.com>

--- componentstore/C2VdecCodecConfig.cpp
+++ componentstore/C2VdecCodecConfig.cpp
@@ -192,8 +192,8 @@ static struct {
     {C2VendorCodec::ADEC_DTSE, kDTSEDecoderName, NULL},
     {C2VendorCodec::ADEC_DTSUHD, kDTSUHDDecoderName, NULL},
     {C2VendorCodec::ADEC_AC4, kAC4DecoderName, NULL},
-    {C2VendorCodec::ADEC_MP1, kMP1DecoderName, NULL},
     {C2VendorCodec::VDEC_JPEG, kJPEGDecoderName, NULL},
+    {C2VendorCodec::ADEC_MP1, kMP1DecoderName, NULL},
 };
 
 static struct {
